dfeb60331178b347eb6b1960d08823432d1f32bc
sindhuraprogrammer <sindhura.programmer@gmail.com>
2020-07-06 14:19:00 +1200

    1 file modified

--- index.js
+++ index.js
@@ -6,34 +6,34 @@ import UserInput from './UserInput/UserInput';
 
 class App extends Component {
   constructor() {
-    super();
-    this.state = {
-      username: 'Sindhu'
-    };
+      super();
+      this.state = {
+          username: 'Sindhu'
+      };
   }
   
-changeUsernameHandler=(event)=>{
-  this.setState({
-username:event.target.value
-})
-}
+  changeUsernameHandler=(event)=>{
+      this.setState({
+          username:event.target.value
+      })
+  }
   teststyleout={
-    color: 'orange',
-    fontFamily: 'Arial'
+      color: 'orange',
+      fontFamily: 'Arial'
   }
   render() {
     const teststylein={
-    color: 'red',
-    fontFamily: 'Arial'
-  }
-    return (
-      <div>
-      <UserInput change={this.changeUsernameHandler} username={this.state.username}/>
-       <UserOutput username={this.state.username}/>
-       <p style={this.teststyleout}>successfully</p>
-       <p style={teststylein}>Completed</p>
-       </div>
-    );
+        color: 'red',
+        fontFamily: 'Arial'
+    }
+     return (
+        <div>
+        <UserInput change={this.changeUsernameHandler} username={this.state.username}/>
+        <UserOutput username={this.state.username}/>
+        <p style={this.teststyleout}>successfully</p>
+        <p style={teststylein}>Completed</p>
+        </div>
+     );
   }
 }
 
